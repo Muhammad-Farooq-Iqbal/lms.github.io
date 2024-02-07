8156d20d1e717431553329f246ce3967280ca1b7
muhammad farooq <farooqkanwal6657@gmail.com>
2024-02-07 09:24:05 +0500

    Updates

---
+++ precache-manifest.c045b9c794daf8be86824a82edf7c54b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "29737a72e323027b4a20",
+    "url": "/lms.github.io/static/js/main.29737a72.chunk.js"
+  },
+  {
+    "revision": "0fcb04df789b7a011bcb",
+    "url": "/lms.github.io/static/js/1.0fcb04df.chunk.js"
+  },
+  {
+    "revision": "b9b6e1d6e3b108c70f81",
+    "url": "/lms.github.io/static/js/runtime~main.b9b6e1d6.js"
+  },
+  {
+    "revision": "a3a78165608438f20ba88ee14ddab866",
+    "url": "/lms.github.io/static/media/age-of-vice-book.a3a78165.png"
+  },
+  {
+    "revision": "8e98cf01c2eee0c03d20b97a97a85c30",
+    "url": "/lms.github.io/static/media/city-under-one-roof-book.8e98cf01.png"
+  },
+  {
+    "revision": "cf6431107c55a82b591bd82d47e4996a",
+    "url": "/lms.github.io/static/media/the-world-and-all-that-it-holds-book.cf643110.png"
+  },
+  {
+    "revision": "aa86dee547f32085907fe1b9bd207e3b",
+    "url": "/lms.github.io/static/media/maame-book.aa86dee5.png"
+  },
+  {
+    "revision": "1be321c9e84a8766d5e6728b236a3eca",
+    "url": "/lms.github.io/static/media/essex-dogs-book.1be321c9.png"
+  },
+  {
+    "revision": "322ce59e201a3e8bc98f82f9f3c6faeb",
+    "url": "/lms.github.io/static/media/what-happened-to-ruthy-ramirez-book.322ce59e.png"
+  },
+  {
+    "revision": "c73e3287d08419dc35ae917bbe44ae24",
+    "url": "/lms.github.io/static/media/a-death-at-the-party-book.c73e3287.png"
+  },
+  {
+    "revision": "1eaded0c1f37428042413b236e953e1b",
+    "url": "/lms.github.io/static/media/the-god-of-endings-book.1eaded0c.png"
+  },
+  {
+    "revision": "8babfec8569e4ee18b6559985056aad3",
+    "url": "/lms.github.io/static/media/now-you-see-us-book.8babfec8.png"
+  },
+  {
+    "revision": "aa04b958ecdacc98240315898be52fe0",
+    "url": "/lms.github.io/static/media/dust-child-book.aa04b958.png"
+  },
+  {
+    "revision": "2d59a5297c1c92d49022e7bde030a11a",
+    "url": "/lms.github.io/static/media/the-mostly-true-story-of-tanner-and-louise-book.2d59a529.png"
+  },
+  {
+    "revision": "1fc0c3f7ccbdc9ada5c1b553218d5779",
+    "url": "/lms.github.io/static/media/homecoming-book.1fc0c3f7.png"
+  },
+  {
+    "revision": "23c3a73787ea80f9099f3698620d0c36",
+    "url": "/lms.github.io/static/media/symphony-of-secrets-book.23c3a737.png"
+  },
+  {
+    "revision": "3a14af439904b7823e85b8afef4fce04",
+    "url": "/lms.github.io/static/media/silver-alert-book.3a14af43.png"
+  },
+  {
+    "revision": "12426938e2b085785ff3e31c14a78625",
+    "url": "/lms.github.io/static/media/only-love-can-hurt-like-this-book.12426938.png"
+  },
+  {
+    "revision": "4a4e699b10e98e812257ef5ade9cb611",
+    "url": "/lms.github.io/static/media/paper-names-book.4a4e699b.png"
+  },
+  {
+    "revision": "e253d51c092ec7e9f0ca4e438ea2d635",
+    "url": "/lms.github.io/static/media/the-collected-regrets-of-clover-book.e253d51c.png"
+  },
+  {
+    "revision": "bfc183b6f0c8e5ae8103b0054a01670b",
+    "url": "/lms.github.io/static/media/yellowface-book.bfc183b6.png"
+  },
+  {
+    "revision": "c27b94aec8672bcce526fae2c56d361e",
+    "url": "/lms.github.io/static/media/the-second-ending-book.c27b94ae.png"
+  },
+  {
+    "revision": "d68fb988f350102f278eb1fc88196d45",
+    "url": "/lms.github.io/static/media/the-wishing-game-book.d68fb988.png"
+  },
+  {
+    "revision": "8a0e9faf801243f9393b38c19269f253",
+    "url": "/lms.github.io/static/media/same-time-next-summer-book.8a0e9faf.png"
+  },
+  {
+    "revision": "747fb2091cb7013def87279055251411",
+    "url": "/lms.github.io/static/media/banyan-moon-book.747fb209.png"
+  },
+  {
+    "revision": "3e5b8b55260570235157f00ec2da5ea6",
+    "url": "/lms.github.io/static/media/crook-manifesto-book.3e5b8b55.png"
+  },
+  {
+    "revision": "0aa0fc37589709f6f060aacd2ad9058f",
+    "url": "/lms.github.io/static/media/everyone-here-is-lying-book.0aa0fc37.png"
+  },
+  {
+    "revision": "7752f2d1e4f84a90e0d1e785e438e18d",
+    "url": "/lms.github.io/static/media/gone-tonight-book.7752f2d1.png"
+  },
+  {
+    "revision": "95c3d38dc17491ecc9e1c6a21540fcd1",
+    "url": "/lms.github.io/index.html"
+  }
+];
